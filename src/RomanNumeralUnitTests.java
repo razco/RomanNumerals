fd38d85f6a8c30b2b2760eedb3053de25f03c631
razco <rcm1_2@hotmail.com>
2012-12-05 20:14:26 +0300

    RED - 4

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -12,6 +12,7 @@ public class RomanNumeralUnitTests {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
 		assertEquals("III", number.toRoman(3));
+		assertEquals("IV", number.toRoman(4));
 	}
 	
 
