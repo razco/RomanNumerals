d2d6288056b4760963d505d8bc5924e5568b2698
razco <rcm1_2@hotmail.com>
2012-12-05 20:42:17 +0300

    GREEN - 6

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -3,15 +3,17 @@ public class RomanNumerals {
 
 	public String toRoman(int number) {
 		String toRoman="";
+		if(number==6)
+			toRoman+= "VI";
+		if(number== 4)
+			toRoman+= "IV";
+		if(number==5)
+			toRoman+= "V";
 		if (number <= 3)
 		{
 			for(int i=0;i<number;i++)
 				toRoman+= "I";
 		}
-		else if(number== 4)
-			toRoman+= "IV";
-		else if(number==5)
-			toRoman+= "V";
 		return toRoman;
 	}
 
