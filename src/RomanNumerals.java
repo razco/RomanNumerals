6c0d8900207fbff057578cedbca88c0398ca70eb
razco <rcm1_2@hotmail.com>
2012-12-05 20:38:47 +0300

    Method change

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,15 +2,17 @@
 public class RomanNumerals {
 
 	public String toRoman(int number) {
-		if (number == 1)
-			return "I";
-		if(number== 2)
-			return "II";
-		if(number== 3)
-			return "III";
-		if(number== 4)
-			return "IV";
-		return "V";
+		String toRoman="";
+		if (number <= 3)
+		{
+			for(int i=0;i<number;i++)
+				toRoman+= "I";
+		}
+		else if(number== 4)
+			toRoman+= "IV";
+		else if(number==5)
+			toRoman+= "V";
+		return toRoman;
 	}
 
 }
